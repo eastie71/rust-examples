550068c6eb98b894da55b65a20a84f642c98f2e3
Craig Eastwood <craigdavideastwood@gmail.com>
2024-11-05 15:49:07 +1100

    Completed Section4 Variable Bindings

---
+++ 4VariableBindings/4-2scope.rs
@@ -0,0 +1,20 @@
+static THIS_ONE: i32 = 10;
+
+fn main() {
+    let shadowed_binding = 1;
+
+    {
+        println!("this one = {}", THIS_ONE);
+        println!("before being shadowed: {}", shadowed_binding);
+
+        // This binding *shadows* the outer one
+        let shadowed_binding = "abc";
+
+        println!("shadowed in inner block: {}", shadowed_binding);
+    }
+    println!("outside inner block: {}", shadowed_binding);
+
+    // This binding *shadows* the previous binding
+    let shadowed_binding = 2;
+    println!("shadowed in outer block: {}", shadowed_binding);
+}
